15846b576a6ab5e4ed2c0a63551346fac1c6bb3d
Luc Dexter <hr021793@live.reading.ac.uk>
2018-01-15 17:45:26 +0000

    play screen component

--- Client-FrontEnd/src/app/app.module.ts
+++ Client-FrontEnd/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 import { SignInComponent } from './play/sign-in/sign-in.component';
 import { PlayComponent } from './play/play.component';
 import { HomeComponent } from './home/home.component';
+import { PlayScreenComponent } from './play/play-screen/play-screen.component';
 
 
 @NgModule({
@@ -14,7 +15,8 @@ import { HomeComponent } from './home/home.component';
     AppComponent,
     SignInComponent,
     PlayComponent,
-    HomeComponent
+    HomeComponent,
+    PlayScreenComponent
   ],
   imports: [
     BrowserModule,
---
+++ Client-FrontEnd/src/app/play/play-screen/play-screen.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-play-screen',
+  templateUrl: './play-screen.component.html',
+  styleUrls: ['./play-screen.component.css']
+})
+export class PlayScreenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
